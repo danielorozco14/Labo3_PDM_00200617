317ac93d25fd81b71e6aae5873708c3a5ce9d85d
danielorozco14 <00200617@uca.edu.sv>
2019-03-22 15:02:24 -0600

    Cambio en manifest, agregado intent filter para dejar la aplicacion como apta para compartir texto plano

---
+++ app/src/main/java/com/example/labo3_pdm/ShareActivity.java
@@ -0,0 +1,22 @@
+package com.example.labo3_pdm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShareActivity extends AppCompatActivity {
+
+    private TextView mtextViewShared;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        mtextViewShared=findViewById(R.id.text_shareActivity);
+        Intent mIntent = getIntent();
+        if (mIntent!=null) {
+            mtextViewShared.setText(mIntent.getStringExtra(Intent.EXTRA_TEXT));
+        }
+    }
+}
